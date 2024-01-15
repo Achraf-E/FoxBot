7fe31cfa84ad390e08978fc43f005c4599421bd9
aelfahem <achraf.elfahem@gmail.com>
2024-01-15 14:14:49 +0100

    ping created

---  src/main.js
+++  src/main.js
@@ -1,8 +1,18 @@
 const Discord = require("discord.js");
-const bot = new Discord.Client({intents : 3276799});
+const intents = new Discord.IntentsBitField(3276799);
+const bot = new Discord.Client({intents});
+const loadCommands = require("./Loaders/loadCommands.js");
 const config = require("./config.js");
 
 bot.login(config.token);
+bot.commands = new Discord.Collection();
+loadCommands(bot);
+
+bot.on("messageCreate", async message => {
+    if (message.content == "!ping"){
+        return bot.commands.get("ping").run(bot, message);
+    }
+});
 
 bot.on("ready", async () => {
     console.log(`${bot.user.username} is online`);
