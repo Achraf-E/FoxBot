515fdc05c8388056d0d9321583192cf00cc113f5
aelfahem <achraf.elfahem@gmail.com>
2024-02-13 17:18:30 +0100

    warnlist finished

---  src/Commands/warnlist.js
+++  src/Commands/warnlist.js
@@ -9,8 +9,19 @@ module.exports = {
     category : "Informations",
     
     async run(bot, interaction, options , db){
-        db.query(`SELECT GROUP_CONCAT(DISTINCT warner) AS warners, count(*) AS count FROM warns GROUP BY warned`, function (err, result, fields){
-            console.log(result);
+        //Create the embed
+        let embed = new Discord.EmbedBuilder()
+        .setColor(bot.color)
+        .setAuthor({ name : bot.user.username, iconURL: bot.user.displayAvatarURL({dynamic: true})})
+        .setTitle(`Liste des warnings dans  ${interaction.guild.name} : `)
+        .setThumbnail(bot.user.displayAvatarURL({dynamic: true}))
+        .setTimestamp();
+                    
+        await db.query(`SELECT warned, GROUP_CONCAT(DISTINCT warner) AS warners, count(*) AS count FROM warns GROUP BY warned` , function (err, result, fields){
+            for(warned of result){
+                embed.addFields({name : `${warned.warned}`, value : `Modos ayants warn : ${warned.warners}, nombre de warns total : ${warned.count}`});
+            }
+            return interaction.reply({embeds: [embed], ephemeral:true});
         });
     }
 }
