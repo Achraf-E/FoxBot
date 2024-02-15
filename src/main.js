af77fa92c0b35b15f2f89e519b8b2bd777387dac
aelfahem <achraf.elfahem@gmail.com>
2024-02-15 12:05:21 +0100

    Prepare for Heroku hosting

--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@ const intents = new Discord.IntentsBitField(3276799);
 const bot = new Discord.Client({intents});
 const loadCommands = require("./Loaders/loadCommands.js");
 const loadEvents = require("./Loaders/loadEvents.js");
-const config = require("./config.json").test;
+const config = process.env.test;
 
-bot.login(require("./config.json").TOKEN);
+bot.login(process.env.TOKEN);
 bot.commands = new Discord.Collection();
 bot.color = "Orange";
 loadCommands(bot,config);
